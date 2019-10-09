50f34330a1bb5cb6d47c95ac268455541583db23
xLPMG <xxlpmghdxx@gmail.com>
2019-10-09 18:57:53 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# premium-tea-shop
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/premium-tea-shop)
---
+++ src/app/products.ts
@@ -0,0 +1,37 @@
+export const products = [
+  {
+    name: 'Black Tea',
+    price: 10,
+    origin: 'China',
+    description: 'Plain black tea',
+    image: 'http://www.pngnames.com/files/4/Black-Tea-Png-Background.png'
+  },
+  {
+    name: 'Green Tea',
+    price: 12,
+    origin: 'China',
+    description: 'Plain green tea',
+    image: '/assets/blacktea.png'
+  },
+  {
+    name: 'English Tea',
+    price: 30,
+    origin: 'England',
+    description: 'Black tea with milk',
+    image: '/assets/blacktea.png'
+  },
+  {
+    name: 'Masala Chai',
+    price: 30,
+    origin: 'India',
+    description: 'Black tea with a mixture of aromatic spices and herbs',
+    image: '/assets/blacktea.png'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
