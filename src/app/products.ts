01d37fa65da14af4acbd283344dc2d608f2ac315
Chuck Norris <Chuck Norris>
2019-10-10 11:51:07 +0200

    änderungfen

--- src/app/products.ts
+++ src/app/products.ts
@@ -11,21 +11,42 @@ export const products = [
     price: 12,
     origin: 'China',
     description: 'Plain green tea',
-    image: '/assets/blacktea.png'
+    image: 'http://www.pngnames.com/files/4/Black-Tea-Png-Background.png'
   },
   {
     name: 'English Tea',
     price: 30,
     origin: 'England',
     description: 'Black tea with milk',
-    image: '/assets/blacktea.png'
+    image: 'http://www.pngnames.com/files/4/Black-Tea-Png-Background.png'
   },
   {
     name: 'Masala Chai',
     price: 30,
     origin: 'India',
     description: 'Black tea with a mixture of aromatic spices and herbs',
-    image: '/assets/blacktea.png'
+    image: 'http://www.pngnames.com/files/4/Black-Tea-Png-Background.png'
+  },
+  {
+    name: 'Some Tea',
+    price: 35,
+    origin: 'Some country',
+    description: 'Just some tea',
+    image: 'http://www.pngnames.com/files/4/Black-Tea-Png-Background.png'
+  },
+  {
+    name: 'Different Tea',
+    price: 44,
+    origin: 'Different country',
+    description: 'tastes differently.',
+    image: 'http://www.pngnames.com/files/4/Black-Tea-Png-Background.png'
+  },
+  {
+    name: 'Expensive Tea',
+    price: 99,
+    origin: 'Heaven',
+    description: 'Just expensive tea',
+    image: 'http://www.pngnames.com/files/4/Black-Tea-Png-Background.png'
   }
 ];
 
