e0b4ad3f7a3b427101ef89a197ef520e826c7d38
Luca G <xxlpmghdxx@gmail.com>
2019-10-14 19:01:01 +0200

    changed assets

--- src/app/products.ts
+++ src/app/products.ts
@@ -4,35 +4,35 @@ export const products = [
     price: 10,
     origin: 'China',
     description: 'Plain black tea',
-    image: 'http://www.pngnames.com/files/4/Black-Tea-Png-Background.png'
+    image: 'assets/black-tea.png'
   },
   {
     name: 'Green Tea',
     price: 12,
     origin: 'China',
     description: 'Plain green tea',
-    image: 'http://www.pngnames.com/files/4/Black-Tea-Png-Background.png'
+    image: 'assets/green-tea.png'
   },
   {
-    name: 'English Tea',
+    name: 'English Breakfast',
     price: 30,
     origin: 'England',
-    description: 'Black tea with milk',
-    image: 'http://www.pngnames.com/files/4/Black-Tea-Png-Background.png'
+    description: 'English breakfast tea is a traditional blend of teas originating from Assam, Ceylon, and Kenya.',
+    image: 'assets/english-breakfast-tea.png'
   },
   {
     name: 'Masala Chai',
     price: 30,
     origin: 'India',
     description: 'Black tea with a mixture of aromatic spices and herbs',
-    image: 'http://www.pngnames.com/files/4/Black-Tea-Png-Background.png'
+    image: 'assets/masala-chai-tea.png'
   },
   {
-    name: 'Some Tea',
+    name: 'Nilgiri',
     price: 35,
-    origin: 'Some country',
-    description: 'Just some tea',
-    image: 'http://www.pngnames.com/files/4/Black-Tea-Png-Background.png'
+    origin: 'India',
+    description: 'A dark, intensely aromatic, fragrant and flavoured tea',
+    image: 'assets/nilgiri-tea.png'
   },
   {
     name: 'Different Tea',
